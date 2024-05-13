2463f20a5996d7ea79db3e856b4517afe9885e11
Valeria <vegavaleriah1@gmail.com>
2024-05-13 13:08:07 -0500

    HOLI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,18 @@
-import Image from 'next/image'
-import Link  from "next/link"
-const Index = () => {
- return (
-    <>
-    <header>
- 
-     <h1> Ejemplo de Imagen</h1>
-    <Image src= "/styles/fondo.jpg"
-      height = {400}
-        width = {600}
-       alt =" Ejemplo de Imagen">
-  </Image>
-  </header>
-      <div>
-        <h1>
-            Pagina de Bienvenida
-        </h1>
-        <p>
-            HOLA KOKUA
-        </p>
-        <p>
-          Ir a la sgte pagina 
-          <Link href= "/PAGE2"> pagina 2</Link>
-        </p>
-        <p>
-          Ir a la imagen
-          <Link href= "/imagen"> Imagen</Link>
-        </p>
-      </div>
-      <div>
-        <ul>
-        <li><Link href= "/xyz/1"> parametro 1</Link></li>
-        <li><Link href= "/xyz/3"> parametro 2</Link></li>
-        <li><Link href= "/xyz/2"> parametro 3</Link></li>
-        <li><Link href= "/xyz/999"> parametro 4</Link></li>
-        <li><Link href= "/xyz/8888"> parametro 5</Link></li>
-        </ul>
-      </div>
+import Link from "next/link";
+import Head from "next/head";
+import Layout from './componentes/Layout.js';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from "next/router";
+function App() {
 
-</>
- )
-}
-export default Index
+  
+  
+    return (
+      <Layout
+        
+      />
+    );
+  }
+  
+
+export default App;
