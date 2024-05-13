529851a1d7f7aef1b155686c16fcba99e1e418d6
AstridM15 <astridmucha6@gmail.com>
2024-05-13 13:19:36 -0500

    vfvfv

--- src/pages/Registrar.js
+++ src/pages/Registrar.js
@@ -1,18 +1,82 @@
-import Link from "next/link";
 import Head from "next/head";
 import Layout from './componentes/Layout.js';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from "next/router";
-function App() {
 
-  
-  
-    return (
-      <Layout
+function App() {
+  const [aceptarPolitica, setAceptarPolitica] = useState(false);
+  const [aceptarTerminos, setAceptarTerminos] = useState(false);
+
+  const handleAceptacionPolitica = () => {
+    setAceptarPolitica(!aceptarPolitica);
+  };
+
+  const handleAceptacionTerminos = () => {
+    setAceptarTerminos(!aceptarTerminos);
+  };
+
+  return (
+    <Layout>
+      <div className="Registrar">
+        <div className="FormularioRegistro">
+          <h1 id="Registrarse">Registrarse</h1>
+          <p>
+            <label htmlFor="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" />
+          </p>
+          <p>
+            <label htmlFor="apellidoPaterno">Apellido Paterno:</label>
+            <input type="text" id="apellidoPaterno" name="apellidoPaterno" />
+          </p>
+          <p>
+            <label htmlFor="apellidoMaterno">Apellido Materno:</label>
+            <input type="text" id="apellidoMaterno" name="apellidoMaterno" />
+          </p>
+          <p>
+            <label htmlFor="correo">Correo electrónico:</label>
+            <input type="text" id="correo" name="correo" />
+          </p>
+          <p>
+            <label htmlFor="contrasena">Contraseña:</label>
+            <input type="password" id="contrasena" name="contrasena" />
+          </p>
+          <p>
+            <label htmlFor="confirmarContrasena">Confirmar contraseña:</label>
+            <input type="password" id="confirmarContrasena" name="confirmarContrasena" />
+          </p>
+          <div className="politica-privacidad">
+            <input
+              type="checkbox"
+              id="aceptarPolitica"
+              name="aceptarPolitica"
+              checked={aceptarPolitica}
+              onChange={handleAceptacionPolitica}
+            />
+            <label1 htmlFor="aceptarPolitica">Acepto la política de privacidad y tratamiento de datos personales</label1>
+          </div>
+          <div className="terminos-condiciones">
+            <input
+              type="checkbox"
+              id="aceptarTerminos"
+              name="aceptarTerminos"
+              checked={aceptarTerminos}
+              onChange={handleAceptacionTerminos}
+            />
+            <label1 htmlFor="aceptarTerminos">Acepto los términos y condiciones</label1>
+          </div>
+          <button className="Botonregistro" disabled={!aceptarPolitica || !aceptarTerminos}>Registrarse</button>
+        </div>
+        <div className="Fotoregistrar">
+          <img src="fotoregistrar.png" alt="Foto de registro" className="foto" />
+        </div>
         
-      />
-    );
-  }
-  
+      </div>
+      <div className="Dudas">
+          <p>Si tienes dudas o consultas, </p>
+          <p>escríbenos a infokokua@gmail.com</p>
+        </div>
+    </Layout>
+  );
+}
 
 export default App;
--- src/pages/componentes/Layout.js
+++ src/pages/componentes/Layout.js
@@ -17,49 +17,49 @@ export default function Layout({ children }) {
                         <div className="Espaciado"></div>
                         <Link href="/IniciarSesion">Iniciar sesión</Link>
                     </div>
-                    <div className="register-button">
+                    <div className="Layout-button">
                         <Link href="/Registrar">Registrarse</Link>
                     </div>
                 </nav>
-                <main>
-                    {children}
-                </main>
             </div>
+            <main>
+                {children}
+            </main>
             <footer>
                 <div className="Footerfeo">
-                    <div className="footerconimagen">
-                        <img src="imagenkokua.png" alt="" className="kokua2" />
-                        <div className="footer-contenido">
-                            <div className="footer-primeracolumna">
-                                <p>Email: voluntarioskokua@gmail.com</p>
-                                <p>Teléfono: 914-123-456</p>
-                                <p>Facebook: KOKUA PERU</p>
-                                <p>Instagram: KOKUA PERU</p>
+                    <div className="footer-contenido">
+                        <div className="footer-primeracolumna">
+                            <div className="footerconimagen">
+                                <img src="imagenkokua.png" alt="" className="kokua2" />
                             </div>
-                            <div className="footer-segundacolumna">
-                                <p>Menú principal</p>
-                                <a href="/Nosotros">Nosotros</a>
-                                <a href="/Organizaciones">Organizaciones</a>
-                            </div>
-                            <div className="footer-segundacolumna">
-                                <p>UWU</p>
-                                <a href="/Preguntas">Preguntas frecuentas</a>
-                                <a href="/Terminos">Terminos y condiciones</a>
-                                <a href="/Politicas">Politicas de privacidad</a>
-                            </div>
-                            <div className="footer-segundacolumna">
-                                <p>ENCUENTRANOS EN </p>
-                                <a href="/Instagram">Instagram</a>
-                                <a href="/Facebook">Facebook</a>
-                                <a href="/Twiter">Twiter</a>
-                            </div>
+                            <p>Email: voluntarioskokua@gmail.com</p>
+                            <p>Teléfono: 914-123-456</p>
+                            <p>Facebook: KOKUA PERU</p>
+                            <p>Instagram: KOKUA PERU</p>
+                        </div>
+                        <div className="footer-segundacolumna">
+                            <p>Menú principal</p>
+                            <a href="/Nosotros">Nosotros</a>
+                            <a href="/Organizaciones">Organizaciones</a>
+                        </div>
+                        <div className="footer-segundacolumna">
+                            <p>UWU</p>
+                            <a href="/Preguntas">Preguntas frecuentas</a>
+                            <a href="/Terminos">Terminos y condiciones</a>
+                            <a href="/Politicas">Politicas de privacidad</a>
+                        </div>
+                        <div className="footer-segundacolumna">
+                            <p>ENCUENTRANOS EN </p>
+                            <a href="/Instagram">Instagram</a>
+                            <a href="/Facebook">Facebook</a>
+                            <a href="/Twiter">Twiter</a>
                         </div>
                     </div>
                 </div>
                 <div className="footercopy">
-                <p> © Copyright (©) 2023 KOKUA. Todos los derechos reservados.</p></div>
+                    <p> © Copyright (©) 2023 KOKUA. Todos los derechos reservados.</p>
+                </div>
             </footer>
-
         </>
     );
 }
