c9595b789a419b320826999eaa55f4e4d8eb6914
Valeria <vegavaleriah1@gmail.com>
2024-05-25 19:40:23 -0500

    as

--- src/pages/IniciarSesion.js
+++ src/pages/IniciarSesion.js
@@ -12,6 +12,10 @@ function App() {
     const handleLogin = async (e) => {
         e.preventDefault();
 
+        if (!correo || !contrasena) {
+            setMensaje('Correo y contraseña son requeridos');
+            return;
+        }
         try {
             const response = await fetch('/api/verificarUsuario');
             const data = await response.json();
